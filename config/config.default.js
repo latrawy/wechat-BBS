a5bc10c5b7595ae22c8233c3b2eca737b9c85418
latrawy <latrawy@163.com>
2019-11-14 16:33:30 +0800

    初始化项目

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+exports.keys = '23141234';
+
+exports.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+        '.tpl': 'nunjucks',
+    },
+};
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
